2c58d68ff0cbaae25858ed4756a6d47c7ca7eb30
Ayabonga <ayabongabooi2@gmail.com>
2022-12-28 22:13:10 +0200

    chore(http_exception): added app HttpException file

---
+++ lib/core/exceptions/http_exception.dart
@@ -0,0 +1,17 @@
+class HttpException implements Exception {
+  /// Creates a new instance of [HttpException]
+  HttpException({
+    this.title,
+    this.message,
+    this.statusCode,
+  });
+
+  /// Exception title
+  final String? title;
+
+  /// Exception message
+  final String? message;
+
+  /// Exception http response code
+  final int? statusCode;
+}
