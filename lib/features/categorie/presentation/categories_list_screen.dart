73f680fb980fe4629bc397120e25c3dc8952110e
Ayabonga <ayabongabooi2@gmail.com>
2022-12-31 21:46:29 +0200

    feat(categories_list_screen): added categories list screen

---
+++ lib/features/categorie/presentation/categories_list_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:opentrivia/features/categorie/presentation/categorie_list/categories_grid.dart';
+
+class CategoriesListScreen extends StatelessWidget {
+  const CategoriesListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Text(
+              "Tap on a category to start the quiz.", // TODO Move this to tranlation file
+              style: TextStyle(
+                // TODO Move style
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                fontSize: 16.0,
+              ),
+            ),
+          ),
+        ),
+        CategoryGrid(),
+      ],
+    );
+  }
+}
