196fd339c79420d974348bdaaaa5ebf823403096
Ayabonga <ayabongabooi2@gmail.com>
2023-01-02 20:37:23 +0200

    feat(quiz_settings): updated QuizSettingsModel and added new field type

--- lib/features/quiz_settings/domain/quiz_settings.dart
+++ lib/features/quiz_settings/domain/quiz_settings.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:opentrivia/features/quiz_settings/presentation/widget/quiz_level/quiz_level.dart';
 
 part 'quiz_settings.freezed.dart';
 part 'quiz_settings.g.dart';
@@ -8,9 +9,16 @@ class QuizSettingsModel with _$QuizSettingsModel {
   const QuizSettingsModel._();
 
   factory QuizSettingsModel({
-    String? difficulty,
+    required String difficulty,
     required int total,
+    required String type,
   }) = _QuizSettingsModel;
+
+  factory QuizSettingsModel.empty() => QuizSettingsModel(
+        difficulty: Difficulty.easy.name,
+        total: 10,
+        type: 'True/False',
+      );
 
   factory QuizSettingsModel.fromJson(Map<String, dynamic> json) =>
       _$QuizSettingsModelFromJson(json);
