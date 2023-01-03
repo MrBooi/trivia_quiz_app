7c28bb587fcfbd6f0a76d2b8fcdd210d74cc6b97
Ayabonga <ayabongabooi2@gmail.com>
2023-01-03 03:19:17 +0200

    feat(quiz_settings_model): updated the quiz settings model

--- lib/features/quiz/domain/i_question_facade.dart
+++ lib/features/quiz/domain/i_question_facade.dart
@@ -1,9 +1,11 @@
 import 'package:opentrivia/features/quiz/domain/question.dart';
+import 'package:opentrivia/features/quiz_settings/presentation/widget/quiz_type/quiz_question_type.dart';
 
 abstract class IQuestionFacade {
   Future<List<Question>> getQuestions({
     required String id,
     required int total,
     required String difficulty,
+    required QuizLevelType type,
   });
 }
