a1dec80dac90c361eef3442cb927c6b55c31e85c
Ayabonga <ayabongabooi2@gmail.com>
2023-01-03 22:23:33 +0200

    refactor(question): updated question model incorrect_answers and append correct optioms on it

--- lib/features/quiz/domain/question.dart
+++ lib/features/quiz/domain/question.dart
@@ -13,11 +13,20 @@ class Question with _$Question {
     required String difficulty,
     required String question,
     @JsonKey(name: "correct_answer") required String correctAnswer,
-    @JsonKey(name: "incorrect_answers") required List<String> incorrectAnswers,
+    @JsonKey(name: "incorrect_answers") required List<String> answers,
   }) = _Question;
 
   factory Question.fromJson(Map<String, dynamic> json) =>
-      _$QuestionFromJson(json);
+      _$QuestionFromJson(_jsonModel(json));
+
+  static Map<String, dynamic> _jsonModel(Map<String, dynamic> json) {
+    return {
+      ...json,
+      'incorrect_answers': List<String>.from(json['incorrect_answers'] ?? [])
+        ..add(json['correct_answer'] ?? '')
+        ..shuffle()
+    };
+  }
 
   static List<Question> fromData(List<Map<String, dynamic>> data) {
     return data.map((question) => Question.fromJson(question)).toList();
