53e13114e23b12796197365fa5efc62e644d5364
Ayabonga <ayabongabooi2@gmail.com>
2023-01-12 01:14:20 +0200

    feat(robot): updated robot to include QuizSettingsRobot

--- test/robot.dart
+++ test/robot.dart
@@ -1,21 +1,40 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:opentrivia/core/presentation/app_widget.dart';
+import 'package:opentrivia/features/categorie/infrastucture/category_facade.dart';
 
 import 'core/shared/shared_robot.dart';
+import 'fixtures/quiz_settings/quiz_settings_fixture.dart';
+import 'fixtures/quiz_settings/quiz_settings_robot.dart';
 
 class Robot {
-  Robot(this.tester) : shared = SharedRobot(tester);
+  Robot(this.tester)
+      : shared = SharedRobot(tester),
+        quizSettingsRobot = QuizSettingsRobot(tester);
   final WidgetTester tester;
   final SharedRobot shared;
+  final QuizSettingsRobot quizSettingsRobot;
 
   Future<void> pumpMyApp() async {
     // * Entry point of the app
     await tester.pumpWidget(
-      const ProviderScope(
-        child: AppWidget(),
+      ProviderScope(
+        overrides: [
+          categoriesListProvider.overrideWith((ref) async {
+            return categoryResponseFixture;
+          }),
+        ],
+        child: const AppWidget(),
       ),
     );
+    await tester.pumpAndSettle();
+  }
+
+  Future<void> tapOnSettingsIcon() async {
+    final finder = find.byType(IconButton);
+    expect(finder, findsOneWidget);
+    await tester.tap(finder);
     await tester.pumpAndSettle();
   }
 }
