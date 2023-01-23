8eed36c312e0f9a832c9ce1bf2b22911d9721b33
Ayabonga <ayabongabooi2@gmail.com>
2023-01-23 16:28:36 +0200

    feat(quiz_flow_test): updated integration test flow

--- integration_test/quiz_flow_test.dart
+++ integration_test/quiz_flow_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
+import '../test/fixtures/quiz_settings/quiz_settings_fixture.dart';
 import '../test/robot.dart';
 
 void main() {
@@ -10,6 +11,26 @@ void main() {
 
       r.categoriesRobot.expectCategoryMainTextFound();
       r.categoriesRobot.expectCategoriesFound();
+
+      // verify on settings screen
+      await r.tapOnSettingsIcon();
+      r.quizSettingsRobot.foundQuizSettings();
+      r.quizSettingsRobot.expectNumberOfQuestionsFound();
+      r.quizSettingsRobot.expectQuizLevelFound();
+      r.quizSettingsRobot.expectNumberOfQuestionsFound();
+
+      // update settings
+      await r.quizSettingsRobot.tapNumberOfQuestions(quizSettingsFixture.total);
+      await r.quizSettingsRobot.tapQuizLevel(quizDifficulty);
+      await r.quizSettingsRobot.tapQuizQuestionType(quizSettingsFixture.type);
+
+      // close settings screen
+      // TODO click on close button
+
+      // // tap on category item
+      // r.categoriesRobot.expectCategoryMainTextFound();
+      // // tap on category item
+      // r.categoriesRobot.tapCategoryItem(categories[3].id.toString());
     });
   });
 }
--- test/fixtures/quiz_settings/quiz_settings_fixture.dart
+++ test/fixtures/quiz_settings/quiz_settings_fixture.dart
@@ -1,4 +1,5 @@
 import 'package:opentrivia/features/quiz_settings/domain/quiz_settings.dart';
+import 'package:opentrivia/features/quiz_settings/presentation/widget/quiz_level/quiz_level.dart';
 import 'package:opentrivia/features/quiz_settings/presentation/widget/quiz_type/quiz_question_type.dart';
 
 QuizSettingsModel get quizSettingsFixture => QuizSettingsModel(
@@ -6,3 +7,5 @@ QuizSettingsModel get quizSettingsFixture => QuizSettingsModel(
       total: 10,
       type: QuizLevelType.multiple,
     );
+
+Difficulty get quizDifficulty => Difficulty.easy;
